fbf3fb7bcc60fb4ef80f66726adf5d3c14a3ac86
RaviFontes <ravi.souza@gmail.com>
2022-05-14 16:26:45 -0300

    Aula Javascript 14.05.2022

---
+++ javascript/1.js
@@ -0,0 +1,52 @@
+//alert("bem vindo!");
+
+/*document.write("Bem vido ao Javascript");
+prompt("Dite seu nome"); //caixa para digitar algo
+resp= confirm("Será que vai chover?"); //caixa de confirmação
+//Alertas 
+alert("Um retorno de carro\bem no meio desta linha");
+alert("\"Isso nao saiu como deveria\"disse ela");
+alert("esta linha tem tabulação\taqui");
+*/
+/*
+var a,b;
+a = 4;
+b = "5 kg de açucar";
+
+document.write("O valor da variável a é: " +a );
+document.write("<p>" +"O valor da variavael B é: "+b+"</p>");
+document.write("<p>" +"O valor da variavael A dividido por 2 é: " +a/2+" </p>");
+document.write("<p>" +"O valor da variavael B dividido por 2 é: " +b/2+" </p>");
+
+*/ 
+/*
+var a, inteiro, flutuante;
+a = "45.78 Kg de açucar"
+inteiro = parseInt(a);//converte para numero Inteiro
+flutuante = parseFloat(a); // converte em numeros flutuantes
+document.write("O valor da variável A é: " +a);
+document.write("<p>" + "O valor inteiro da variavel é: " +inteiro+"</p>");
+document.write("<p>" + "O valor inteiro da variavel A dividido por 2 é: " +inteiro/2+"</p>");
+document.write("<p>" + "O valor da variavel decimal da variavel é: " +flutuante+"</p>");
+*/
+/*
+var a, b, c;
+a= "Olá pessoal ";
+b= "Seja bem vindo ao Javascript";
+c= a+b;
+alert(c);
+*/ 
+/*
+a = 50/2;
+b = 100/5;
+c = 25/2;
+d = 50-50;
+e = 50%2; 
+
+document.write("<p>" + a +"</p>"); 
+document.write("<p>" + b +"</p>");
+document.write("<p>" + c +"</p>");
+document.write("<p>" + d +"</p>");
+document.write("<p>" + e +"</p>");
+*/ 
+
